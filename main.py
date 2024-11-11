6afe90fd5ee292be4f469b9b9e331a42dfb89082
KallinHiatt <kallin.hiatt@ucas-edu.net>
2024-11-11 11:49:48 -0700

    Pipes and collisions

--- main.py
+++ main.py
@@ -6,6 +6,10 @@ from game import Game
 pygame.init()
 screen = pygame.display.set_mode((400, 720))
 clock = pygame.time.Clock()
+
+SPAWNPIPE = pygame.USEREVENT
+pygame.time.set_timer(SPAWNPIPE, 2000)
+
 game = Game('img/bird.png', 'img/pipe.png', 'img/background.png', 'img/ground.png')
 game.resize_images()
 
@@ -19,6 +23,10 @@ while True:
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_SPACE and game.active:
                 game.flap()
+        
+
+        if event.type == SPAWNPIPE:
+            game.add_pipe()
 
 
 
@@ -27,9 +35,13 @@ while True:
     if game.active:
         game.show_bird(screen)
         game.update_bird()
+        game.move_pipe()
+        game.show_pipes(screen)
+        game.check_collision()
 
     game.show_ground(screen)
     game.move_ground()
 
     pygame.display.update()
     clock.tick(120)
+     
